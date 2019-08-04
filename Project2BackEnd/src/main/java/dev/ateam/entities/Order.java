6b07f7080517472a8c64cde833492b2a741bc0f6
Nick <nickpaolella45@gmail.com>
2019-08-04 15:58:43 -0400

    Order debugged. It compiles

--- Project2BackEnd/src/main/java/dev/ateam/entities/Order.java
+++ Project2BackEnd/src/main/java/dev/ateam/entities/Order.java
@@ -1,12 +1,21 @@
 package dev.ateam.entities;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "computer_order")
 public class Order {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="order_id")
 	private int orderId;
 	
